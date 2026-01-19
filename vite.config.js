0c26ba0d191309f96f14c76a1304628a3835f656
ilyashirko <ilyashirko@gmail.com>
2026-01-19 13:20:00 +0300

    add allowed hosts

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // слушать на всех интерфейсах
+    allowedHosts: ["develop.ilyashirko.dev", "localhost", "127.0.0.1"],
+    port: 5173,
+  },
 })
