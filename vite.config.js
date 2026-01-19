a14f837e1bdfe09a540786b3054a519403e302c2
ilyashirko <ilyashirko@gmail.com>
2026-01-19 13:50:22 +0300

    change height

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,18 @@
+  GNU nano 6.2                                                                                                                                                  vite.config.js                                                                                                                                                           
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/eis9pheGoeyaepaiw0oc/",
   server: {
     host: true, // слушать на всех интерфейсах
     allowedHosts: ["develop.ilyashirko.dev", "localhost", "127.0.0.1"],
     port: 5173,
+    strictPort: true,
+    hmr: {
+      host: 'develop.ilyashirko.dev',
+    },
   },
 })
